203771bdaac23018fd5aa77badb8c91e337f38db
Ashu9363 <121708423+Ashu9363@users.noreply.github.com>
2023-08-20 22:13:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const fs = require('fs')
+const express = require('express');
+const path = require('path'); // Import the 'path' module
+const app = express();
+const port = process.env.PORT || 3000; // Use the environment port or default to 3000
 
 // Serve static files from the "public" directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/home', (req, res) => {
-    res.statusCode = 200
-  const data = fs.readFileSync('index.html')
-    res.send(data.toString())
-})
+  res.sendFile(path.join(__dirname,  'index.html'));
+});
+
 app.get('/about', (req, res) => {
-    res.statusCode = 200
-    const data = fs.readFileSync('about.html')
-      res.send(data.toString())
-  })
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('*', (req, res) => {
+  res.statusCode = 404;
+  res.send('404 - Page not found');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}/home`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
